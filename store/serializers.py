86590457b29b1e32d7000122e7589efd6735600e
Adrian Kokai <adriankokai@gmail.com>
2021-04-19 14:47:27 +0200

    create a ProductSerializer

---
+++ store/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Product, ProductImage
+
+class ProductImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImage
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    ProductImages = ProductImageSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
