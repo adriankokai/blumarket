31078236a2b9cfcd86d17f6053a60fead257df54
Adrian Kokai <adriankokai@gmail.com>
2021-04-19 14:46:47 +0200

    create 'product' api view that returns the first product in the database

--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def place_order(request):
+    pass
+
+@api_view(['GET'])
+def product(request):
+    product = Product.objects.all()[0]
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
