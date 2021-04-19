418fd95891ffcf9a57a1a2ec36090b04c8a17efd
Adrian Kokai <adriankokai@gmail.com>
2021-04-19 14:44:11 +0200

    register models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ProductImage, Order, Txn
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Order)
+admin.site.register(Txn)
