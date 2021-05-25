19dbcc6a288b8e4b1222d536cfa8f8894e7b4f67
Adrian Kokai <adriankokai@gmail.com>
2021-05-25 16:05:55 +0200

    change settings

--- blumarket/settings.py
+++ blumarket/settings.py
@@ -104,6 +104,15 @@ DATABASES = {
 db_from_env = dj_database_url.config(heroku_db_uri, conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
