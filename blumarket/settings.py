ba618a488f3d12d42a7eb54aeb1c9c52f50c12b0
Adrian Kokai <adriankokai@gmail.com>
2021-05-25 21:04:20 +0200

    change settings

--- blumarket/settings.py
+++ blumarket/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 #from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
-#import os
+import os
 
-load_dotenv()
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
